25a130524cc5e059500cdc45b0fddc4586e917b1
Eugene Theriault <hybridbassist@gmail.com>
2020-10-23 08:40:39 -0700

    Create relations and spec for mechanics index page

---
+++ spec/features/mechanics/index_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe 'When I visit a mechanics index page' do
+  describe 'I see a header saying All Mechanics' do
+    it 'and I see a list of all mechanics and their years experience' do
+      park = Park.create!(name: 'Disneyland')
+      mechanic1 = park.mechanics.create(name: 'Jim Jones',
+                            years_experience: 10)
+      mechanic2 = park.mechanics.create(name: 'George Gin',
+                            years_experience: 5)
+      mechanic3 = park.mechanics.create(name: 'Petra Perogi',
+                            years_experience: 1)
+
+      visit '/mechanics'
+
+      expect(page).to have_content('All Mechanics')
+      expect(page).to have_content(mechanic1.name)
+      expect(page).to have_content(mechanic1.years_experience)
+      expect(page).to have_content(mechanic2.name)
+      expect(page).to have_content(mechanic2.years_experience)
+      expect(page).to have_content(mechanic3.name)
+      expect(page).to have_content(mechanic3.years_experience)
+    end
+  end
+end
