00a452ec14a95df7b312c5b70c3c1e2953756ea8
Eugene Theriault <hybridbassist@gmail.com>
2020-10-23 09:27:19 -0700

    Create tests for relationship between mechanic and ride

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 describe Mechanic, type: :model do
   describe 'relationships' do
     it { should belong_to :park }
+    it { should have_many(:rides).through(:mechanic_rides) }
   end
 end
