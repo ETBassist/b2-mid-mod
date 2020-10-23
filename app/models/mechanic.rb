9785f610321491e69f7becf8f26cebb4eb7b40e6
Eugene Theriault <hybridbassist@gmail.com>
2020-10-23 09:29:11 -0700

    Create relationships between mechanic and ride

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,3 +1,5 @@
 class Mechanic < ApplicationRecord
   belongs_to :park
+  has_many :mechanic_rides
+  has_many :rides, through: :mechanic_rides
 end
--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 describe Ride, type: :model do
   describe 'relationships' do
     it { should belong_to :park }
-    it { should have_many(:rides).through(:mechanic_rides) }
+    it { should have_many(:mechanics).through(:mechanic_rides) }
   end
 end
