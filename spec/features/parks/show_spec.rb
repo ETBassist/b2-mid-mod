47acb90281803d5cd247b088b815d6adc74d75d3
Eugene Theriault <hybridbassist@gmail.com>
2020-10-23 08:55:10 -0700

    Create Ride model and migration, test for relation

---
+++ spec/features/parks/show_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe 'When I visit a parks show page' do
+  describe 'I see details about' do
+    xit
+    it 'the name and price of admissions for that amusement park' do
+    end
+  end
+end
