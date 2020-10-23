628d2543d60ff9f1c55bd7f6a47c337708295363
Eugene Theriault <hybridbassist@gmail.com>
2020-10-23 09:10:47 -0700

    Create test for average_thrill park method

--- spec/models/park_spec.rb
+++ spec/models/park_spec.rb
@@ -5,4 +5,14 @@ describe Park, type: :model do
     it { should have_many :mechanics }
     it { should have_many :rides }
   end
+
+  describe 'class methods' do
+    it 'can get the average thrill rating of rides' do
+      park = Park.create!(name: 'Cedar Rapids', price: 25.0)
+      park.rides.create!(name: 'Lightning Racer', thrill_rating: 10)
+      park.rides.create!(name: 'Storm Runner', thrill_rating: 5)
+      
+      expect(park.average_thrill).to eq(7.5)
+    end
+  end
 end
